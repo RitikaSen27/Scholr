6f9b12c491c8f501b3449b233ad4fd9275f6cb0b
Aritra Majumdar <123831279+AritrMajumda@users.noreply.github.com>
2026-09-07 14:58:31 +0530

    Update api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 
 const api = axios.create({
   baseURL: BASE_URL,
